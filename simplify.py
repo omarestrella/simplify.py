64e95441e25465082c634615dfeb313215861c27
Omar Estrella <gzusfr3ak@gmail.com>
2012-04-18 16:16:40 -0400

    Initial commit

---
+++ simplify.py
@@ -0,0 +1,14 @@
+def getSquareDistance(p1, p2):
+    pass
+
+def getSquareSegmentDistance(p, p1, p2):
+    pass
+
+def simplifyRadialDistance(points, tolerance):
+    pass
+
+def simplifyDouglasPeucker(points, tolerance):
+    pass
+
+def simplify(points, tolerance, highestQuality):
+    pass
